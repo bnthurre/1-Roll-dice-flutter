31ec9f311ae8f5e56776247108fba2db1b278b4f
bnthurre <bushrahurre959@gmail.com>
2024-06-13 09:27:32 +0300

    Working with Constructor Functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
-    home: Scaffold(
-      body: GradientContainer()
-    ),
+  runApp(MaterialApp(
+    home: Scaffold(body: GradientContainer()),
   ));
 }
 
-
-class GradientContainer extends StatelessWidget{
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
   @override
-  Widget build(context){
-    return  Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color.fromARGB(255, 3, 70, 126),
-            Color.fromARGB(255, 6, 41, 70)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-          )
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color.fromARGB(255, 3, 70, 126),
+        Color.fromARGB(255, 6, 41, 70)
+      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+      child: const Center(
+        child: Text(
+          "Bushra Hurre",
+          style: TextStyle(color: Colors.white, fontSize: 28),
         ),
-        child: const Center(
-          child: Text("Bushra Hurre",style: TextStyle(color: Colors.white, fontSize: 28),),
-        ),
-      );
+      ),
+    );
   }
 }
