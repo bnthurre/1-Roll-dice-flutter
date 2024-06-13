6c493dac6a5a3845cd049fc35d922cd135f7186e
bnthurre <bushrahurre959@gmail.com>
2024-06-13 09:30:55 +0300

    Splitting Code Across Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:roll_dice/gradient_container.dart';
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(body: GradientContainer()),
   ));
 }
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-        Color.fromARGB(255, 3, 70, 126),
-        Color.fromARGB(255, 6, 41, 70)
-      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-      child: const Center(
-        child: Text(
-          "Bushra Hurre",
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
-      ),
-    );
-  }
-}
