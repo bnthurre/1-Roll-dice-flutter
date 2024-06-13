bc74cb9f52ae5533a1efbf86431433faf87b7a31
bnthurre <bushrahurre959@gmail.com>
2024-06-13 09:01:43 +0300

    styling text

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,13 @@ void main() {
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [Color.fromARGB(255, 3, 70, 126),
-            Color.fromARGB(255, 6, 41, 70)]
+            Color.fromARGB(255, 6, 41, 70)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
           )
         ),
         child: const Center(
-          child: Text("Bushra Hurre"),
+          child: Text("Bushra Hurre",style: TextStyle(color: Colors.white, fontSize: 28),),
         ),
       ),
     ),
