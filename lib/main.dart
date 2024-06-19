7d3bb098a5a27d7c06690de3a136a520e26aa115
bnthurre <bushrahurre959@gmail.com>
2024-06-19 08:54:40 +0300

    making reusable widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:roll_dice/gradient_container.dart';
 void main() {
   runApp(const MaterialApp(
-    home: Scaffold(body: GradientContainer()),
+    home: Scaffold(body: GradientContainer(
+        Color.fromARGB(255, 3, 70, 126),
+        Color.fromARGB(255, 6, 41, 70)
+      )),
   ));
 }
 
